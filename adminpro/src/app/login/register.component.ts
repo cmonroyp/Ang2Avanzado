216146eb51bb622f7f65774c104643ecf34a8d3b
PC-CMONROY\Cmonroyp <carlosmonroy@outlook.com>
2018-06-11 15:29:58 -0500

    Actualizacion angular V6

--- adminpro/src/app/login/register.component.ts
+++ adminpro/src/app/login/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, group } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FormGroup, FormControl,Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
